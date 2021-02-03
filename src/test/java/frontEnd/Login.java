149b4c85f6c51bd48e60bf543578446400770744
sobirjon001 <sobirjon001@gmail.com>
2021-02-02 21:16:22 -0500

    Endin created update profile functionality

--- src/test/java/frontEnd/Login.java
+++ src/test/java/frontEnd/Login.java
@@ -4,5 +4,6 @@ public class Login {
   public static void main(String[] args) {
     // Sobir created login function
     // Sobir created create new user function
+    // Engin created update profile function
   }
 }
